fe5ea146f0f500be8a19aa531572c074476798ca
andresfgarcia150 <andrespipe150@gmail.com>
2013-03-02 16:31:11 -0500

    Vectores propios ordenados y normalizados

--- tarea3Part2.c
+++ tarea3Part2.c
@@ -17,11 +17,13 @@ void contarDatos(int *nDatos, char nombreArchivo[]);
 gsl_vector* cargarVector(int nDatos, int numeroColumna, char nombreArchivo[]);
 double calcularMedia(int tamanioVector, gsl_vector *vector);
 double calcularCovarianza(int tamanioVector, gsl_vector *v1, gsl_vector *v2, double u1, double u2);
+double calcularNorma(int tamanioVector, gsl_vector *vector);
 
 int main (int argc, char **argv)
 {
 	// Variables de los vectores
 	int nDatos = 0;
+	int nVariables = 3;
 
 	// Revisa que la entrada sea correcta
 	if (argc !=2)
@@ -65,7 +67,7 @@ int main (int argc, char **argv)
 	o23 = calcularCovarianza(nDatos, v2, v3, u2, u3);
 	o33 = calcularCovarianza(nDatos, v3, v3, u3, u3);
 
-	gsl_matrix *mCov = gsl_matrix_calloc(3,3);
+	gsl_matrix *mCov = gsl_matrix_calloc(nVariables, nVariables);
 	gsl_matrix_set(mCov, 0, 0, o11);
 	gsl_matrix_set(mCov, 0, 1, o12);
 	gsl_matrix_set(mCov, 0, 2, o13);
@@ -76,34 +78,85 @@ int main (int argc, char **argv)
 	gsl_matrix_set(mCov, 2, 1, o23);
 	gsl_matrix_set(mCov, 2, 2, o33);
 
-	// Matriz con valores propios y vectores propios
-	gsl_vector *eval = gsl_vector_alloc (3);
-        gsl_matrix *evec = gsl_matrix_alloc (3, 3);
+	// Matriz con vectores propios y vector con valores propios
+	gsl_vector *eval = gsl_vector_alloc (nVariables);
+        gsl_matrix *evec = gsl_matrix_alloc (nVariables, nVariables);
      
-        gsl_eigen_symmv_workspace * w = gsl_eigen_symmv_alloc (3);
+        gsl_eigen_symmv_workspace * w = gsl_eigen_symmv_alloc (nVariables);
        
         gsl_eigen_symmv (mCov, eval, evec, w);
      
         gsl_eigen_symmv_free (w);
 	
 	gsl_eigen_symmv_sort (eval, evec, GSL_EIGEN_SORT_ABS_ASC);
-       
-       {
-         int i;
-     
-         for (i = 0; i < 3; i++)
-           {
-             double eval_i 
-                = gsl_vector_get (eval, i);
-             gsl_vector_view evec_i 
-                = gsl_matrix_column (evec, i);
-     
-             printf ("eigenvalue = %g\n", eval_i);
-             printf ("eigenvector = \n");
-             gsl_vector_fprintf (stdout, 
-                                 &evec_i.vector, "%g");
-           }
-       }
+
+{
+int i;
+for (i = 0; i < nVariables; i++)
+{
+double eval_i
+= gsl_vector_get (eval, i);
+gsl_vector_view evec_i = gsl_matrix_column (evec, i);
+printf ("eigenvalue = %g\n", eval_i);
+printf ("eigenvector = \n");
+gsl_vector_fprintf (stdout,
+&evec_i.vector, "%g");
+}
+}
+
+       	// Ordenamiento de los vectores propios según los valores propios
+	// Algoritmo de selección
+	int i = 0;
+	for (i = 0; i < 2; i++)
+	{
+		double mayorValor = gsl_vector_get(eval,i);
+		int posMayor = i;
+		int j = i+1;
+		for (j=i+1; j<3; j++)
+		{
+			if (mayorValor < gsl_vector_get(eval,j))
+			{
+				posMayor = j;
+				mayorValor = gsl_vector_get(eval,j);
+			}
+		}
+		gsl_vector_swap_elements(eval, i, posMayor);
+		gsl_matrix_swap_columns(evec, i, posMayor);
+	}
+	
+
+	{
+int i;
+for (i = 0; i < nVariables; i++)
+{
+double eval_i
+= gsl_vector_get (eval, i);
+gsl_vector_view evec_i = gsl_matrix_column (evec, i);
+printf ("eigenvalue = %g\n", eval_i);
+printf ("eigenvector = \n");
+gsl_vector_fprintf (stdout,
+&evec_i.vector, "%g");
+}
+}
+	// Normalización y de los vectores propios
+	gsl_vector *vx1 = gsl_vector_calloc (nVariables);
+	gsl_vector *vx2 = gsl_vector_calloc (nVariables);
+	gsl_vector *vx3 = gsl_vector_calloc (nVariables);
+
+	gsl_matrix_get_col(vx1, evec, 0);
+	gsl_matrix_get_col(vx2, evec, 1);
+	gsl_matrix_get_col(vx3, evec, 2);
+	
+	double norm1, norm2, norm3;
+	norm1 = calcularNorma(nVariables, vx1);
+	norm2 = calcularNorma(nVariables, vx2);
+	norm3 = calcularNorma(nVariables, vx3);
+	
+	gsl_vector_scale(vx1,1/norm1);
+	gsl_vector_scale(vx2,1/norm2);
+	gsl_vector_scale(vx3,1/norm3);
+
+	
 }
 
 // Función que cuenta el número de datos a procesar
@@ -177,5 +230,17 @@ double calcularCovarianza(int tamanioVector, gsl_vector *v1, gsl_vector *v2, dou
 		suma += (gsl_vector_get(v1,i)-u1) * (gsl_vector_get(v2,i)-u2);
 	}
 	return suma/(tamanioVector-1);
+}
+
+// Calcula la norma de un vector
+double calcularNorma(int tamanioVector, gsl_vector *vector)
+{
+	double suma = 0;
+	int i = 0;
+	for (i = 0; i < tamanioVector; i++)
+	{
+		suma += pow((gsl_vector_get(vector,i)),2);
+	}
+	return pow(suma,0.5);
 }
 
