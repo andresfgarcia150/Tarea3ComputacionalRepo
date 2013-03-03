3ad4f1c0d6b80c228204a2bf6f660d623b6a85b8
andresfgarcia150 <andrespipe150@gmail.com>
2013-03-02 21:53:06 -0500

    Directorio con los códigos finales de la Tarea 3

---
+++ lin_algebra/tarea3.c
@@ -0,0 +1,200 @@
+/**
+ * Universidad de los Andes
+ * Física computacional
+ * Tarea 3
+ * Autor: Andrés Felipe García Albarracín
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <gsl/gsl_linalg.h>
+//#include <gsl/gsl_matrix.h>
+
+void contarDatos(int *nDatos, char nombreArchivo[]);
+double* cargarVector(int nDatos, int numeroColumna, char nombreArchivo[]);
+
+int main (int argc, char **argv)
+{
+	// Variables de los vectores
+	int nDatos = 0;
+
+	// Revisa que la entrada sea correcta
+	if (argc !=2)
+	{
+		printf("Debe ingresar el nombre del archivo a analizar\n");
+		exit(1);
+	}
+		
+	// Variable del archivo
+	char nombreArchivo[256];
+	strcpy(nombreArchivo,argv[1]);
+	
+
+	// Cuenta el número de datos
+	contarDatos(&nDatos, nombreArchivo);
+	
+	// Arreglos con los vectores a trabajar
+	double* vectorTiempo;
+	vectorTiempo = cargarVector(nDatos, 1, nombreArchivo);
+
+	double* vectorPosicion;
+	vectorPosicion = cargarVector(nDatos, 2, nombreArchivo);
+	
+	
+	// Vectores para la librería GSL
+	gsl_vector *vTiempo = gsl_vector_calloc(nDatos);
+	gsl_vector *vPosicion = gsl_vector_calloc(nDatos);
+	int i = 0;
+	for (i = 0; i < nDatos; i++)
+	{
+		gsl_vector_set(vTiempo,i,vectorTiempo[i]);
+		gsl_vector_set(vPosicion, i, vectorPosicion[i]);
+	}
+
+	// Matriz g y su transpuesta gt
+	int nColumnas = 3; // Coincide con las variables de la regresión
+	int nFilas = nDatos;
+	gsl_matrix *g = gsl_matrix_calloc(nFilas,nColumnas);
+	gsl_matrix *gt = gsl_matrix_calloc(nColumnas,nFilas); //En el orden contrario
+	for (i = 0; i < nFilas; i++)
+	{
+		// Matriz g		
+		gsl_matrix_set(g, i, 0, 1);
+		gsl_matrix_set(g, i, 1, gsl_vector_get(vTiempo,i));
+		gsl_matrix_set(g, i, 2, (gsl_vector_get(vTiempo,i)) * (gsl_vector_get(vTiempo,i)) /2 );
+		// Matriz gt
+		gsl_matrix_set(gt, 0, i, 1);
+		gsl_matrix_set(gt, 1, i, gsl_vector_get(vTiempo,i));
+		gsl_matrix_set(gt, 2, i, (gsl_vector_get(vTiempo,i)) * (gsl_vector_get(vTiempo,i)) /2 );
+		
+	}
+	
+	// Matriz x = gt*g, matriz nxn
+	gsl_matrix *x = gsl_matrix_calloc(nColumnas,nColumnas);
+	int j = 0;
+	int h = 0;
+	double suma = 0;
+	for (i = 0; i < nColumnas; i++) //Filas
+	{
+		for (j = 0; j < nColumnas; j++) // Columnas
+		{
+			suma = 0;
+			for (h = 0; h < nFilas; h++)
+			{
+				suma += gsl_matrix_get(gt,i,h)*gsl_matrix_get(g,h,j);
+			}
+			gsl_matrix_set(x, i, j, suma);
+		}
+	}
+	
+
+	// Matriz inversa de x => xinv
+	gsl_matrix *xinv = gsl_matrix_calloc(nColumnas,nColumnas);
+	gsl_permutation *perm = gsl_permutation_alloc(nColumnas);
+
+		// Descomposicion LU
+	int ent;
+	gsl_linalg_LU_decomp(x,perm,&ent);
+
+		// Inversa
+	gsl_linalg_LU_invert(x,perm,xinv);
+
+
+	// Matriz y = xinv * gt
+	gsl_matrix *y = gsl_matrix_calloc(nColumnas,nFilas);
+	j = 0;
+	h = 0;
+	suma = 0;
+	for (i = 0; i < nColumnas; i++) //Filas
+	{
+		for (j = 0; j < nFilas; j++) // Columnas
+		{
+			suma = 0;
+			for (h = 0; h < nColumnas; h++)
+			{
+				suma += gsl_matrix_get(xinv,i,h)*gsl_matrix_get(gt,h,j);
+			}
+			gsl_matrix_set(y, i, j, suma);
+		}
+	}
+
+	// Vector de salida m
+	gsl_vector *m = gsl_vector_calloc(nColumnas);
+	j = 0;
+	h = 0;
+	suma = 0;
+	for (i = 0; i < nColumnas; i++) // Filas
+	{
+		suma = 0;
+		for (h = 0; h < nFilas; h++)
+		{
+			suma += gsl_matrix_get(y,i,h)*gsl_vector_get(vPosicion,h);
+		}
+		gsl_vector_set(m, i, suma);
+	}
+	
+
+	for (i = 0; i<nColumnas; i++)
+	{
+		printf("%f\n", gsl_vector_get(m, i));
+	}
+	
+	// Archivo de salida
+	FILE *archivoSalida;
+	archivoSalida = fopen("parametros_movimiento.dat","w");
+	fprintf(archivoSalida, "%f %f %f", gsl_vector_get(m,0), gsl_vector_get(m,1), gsl_vector_get(m,2));
+	fclose(archivoSalida);
+	return 0;
+}
+
+void contarDatos(int *nDatos, char nombreArchivo[])
+{
+	// Variables locales y abre el archivo
+	int numDatos = 0;
+	FILE *archivo;
+	archivo = fopen(nombreArchivo,"r");
+
+
+	char var = fgetc(archivo);
+	// Cuenta el número de datos
+	while (var != EOF)
+	{
+		if (var == '\n')
+			numDatos++;
+		var = fgetc(archivo);
+	}
+	
+	*nDatos = numDatos;
+	fclose(archivo);
+}
+
+double* cargarVector(int nDatos, int numeroColumna, char nombreArchivo[])
+{
+	// Variables locales y abre el archivo
+	FILE *archivo2;
+	archivo2 = fopen(nombreArchivo,"r");
+
+	// Arreglo con los datos de la matriz
+	double *vector;
+	vector = malloc((nDatos)*sizeof(double));
+
+	// Lee los datos y los guarda en la matriz
+	int filaActual = 0;
+	for (filaActual = 0; filaActual < nDatos; filaActual++)
+	{
+		double dato1;
+		double dato2;
+		fscanf(archivo2, "%lf %lf\n", &dato1, &dato2);
+		if (numeroColumna == 1)
+			vector[filaActual] = dato1;
+		else if (numeroColumna == 2)
+			vector[filaActual] = dato2;
+	}
+
+	fclose(archivo2);		
+	return vector;
+}
+
+
+
---
+++ lin_algebra/tarea3Part2.c
@@ -0,0 +1,224 @@
+/**
+ * Universidad de los Andes
+ * Física computacional
+ * Tarea 3 - Parte 2
+ * Autor: Andrés Felipe García Albarracín
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_eigen.h>
+//#include <gsl/gsl_matrix.h>
+
+void contarDatos(int *nDatos, char nombreArchivo[]);
+gsl_vector* cargarVector(int nDatos, int numeroColumna, char nombreArchivo[]);
+double calcularMedia(int tamanioVector, gsl_vector *vector);
+double calcularCovarianza(int tamanioVector, gsl_vector *v1, gsl_vector *v2, double u1, double u2);
+double calcularNorma(int tamanioVector, gsl_vector *vector);
+
+int main (int argc, char **argv)
+{
+	// Variables de los vectores
+	int nDatos = 0;
+	int nVariables = 3;
+
+	// Revisa que la entrada sea correcta
+	if (argc !=2)
+	{
+		printf("Debe ingresar el nombre del archivo a analizar\n");
+		exit(1);
+	}
+		
+	// Variable del archivo
+	char nombreArchivo[256];
+	strcpy(nombreArchivo,argv[1]);
+	
+	// Cuenta el número de datos
+	contarDatos(&nDatos, nombreArchivo);
+	
+	// Lee los datos del archivo
+	gsl_vector *v1 = gsl_vector_calloc(nDatos);
+	v1 = cargarVector(nDatos, 1, nombreArchivo);
+	//gsl_vector_fprintf (stdout, v1, "%g");
+
+	gsl_vector *v2 = gsl_vector_calloc(nDatos);
+	v2 = cargarVector(nDatos, 2, nombreArchivo);
+	//gsl_vector_fprintf (stdout, v2, "%g");
+
+	gsl_vector *v3 = gsl_vector_calloc(nDatos);
+	v3 = cargarVector(nDatos, 3, nombreArchivo);
+	//gsl_vector_fprintf (stdout, v3, "%g");
+	
+	// Medias aritméticas
+	double u1, u2, u3;
+	u1 = calcularMedia(nDatos, v1);
+	u2 = calcularMedia(nDatos, v2);
+	u3 = calcularMedia(nDatos, v3);
+
+	// Matriz de covarianza
+	double o11, o12, o13, o22, o23, o33;
+	o11 = calcularCovarianza(nDatos, v1, v1, u1, u1);
+	o12 = calcularCovarianza(nDatos, v1, v2, u1, u2);
+	o13 = calcularCovarianza(nDatos, v1, v3, u1, u3);
+	o22 = calcularCovarianza(nDatos, v2, v2, u2, u2);
+	o23 = calcularCovarianza(nDatos, v2, v3, u2, u3);
+	o33 = calcularCovarianza(nDatos, v3, v3, u3, u3);
+
+	gsl_matrix *mCov = gsl_matrix_calloc(nVariables, nVariables);
+	gsl_matrix_set(mCov, 0, 0, o11);
+	gsl_matrix_set(mCov, 0, 1, o12);
+	gsl_matrix_set(mCov, 0, 2, o13);
+	gsl_matrix_set(mCov, 1, 0, o12);
+	gsl_matrix_set(mCov, 1, 1, o22);
+	gsl_matrix_set(mCov, 1, 2, o23);
+	gsl_matrix_set(mCov, 2, 0, o13);
+	gsl_matrix_set(mCov, 2, 1, o23);
+	gsl_matrix_set(mCov, 2, 2, o33);
+
+	// Matriz con vectores propios y vector con valores propios
+	gsl_vector *eval = gsl_vector_alloc (nVariables);
+        gsl_matrix *evec = gsl_matrix_alloc (nVariables, nVariables);
+     
+        gsl_eigen_symmv_workspace * w = gsl_eigen_symmv_alloc (nVariables);
+       
+        gsl_eigen_symmv (mCov, eval, evec, w);
+     
+        gsl_eigen_symmv_free (w);
+	
+	gsl_eigen_symmv_sort (eval, evec, GSL_EIGEN_SORT_ABS_ASC);
+
+
+       	// Ordenamiento de los vectores propios según los valores propios
+	// Algoritmo de selección
+	int i = 0;
+	for (i = 0; i < 2; i++)
+	{
+		double mayorValor = gsl_vector_get(eval,i);
+		int posMayor = i;
+		int j = i+1;
+		for (j=i+1; j<3; j++)
+		{
+			if (mayorValor < gsl_vector_get(eval,j))
+			{
+				posMayor = j;
+				mayorValor = gsl_vector_get(eval,j);
+			}
+		}
+		gsl_vector_swap_elements(eval, i, posMayor);
+		gsl_matrix_swap_columns(evec, i, posMayor);
+	}
+	
+
+	// Normalización de los vectores propios (No es necesario: atrás ya están normalizados)
+	gsl_vector *vx1 = gsl_vector_calloc (nVariables);
+	gsl_vector *vx2 = gsl_vector_calloc (nVariables);
+	gsl_vector *vx3 = gsl_vector_calloc (nVariables);
+
+	gsl_matrix_get_col(vx1, evec, 0);
+	gsl_matrix_get_col(vx2, evec, 1);
+	gsl_matrix_get_col(vx3, evec, 2);
+	
+	double norm1, norm2, norm3;
+	norm1 = calcularNorma(nVariables, vx1);
+	norm2 = calcularNorma(nVariables, vx2);
+	norm3 = calcularNorma(nVariables, vx3);
+
+	// Archivo de salida
+	FILE *archivoSalida;
+	archivoSalida = fopen("autovectores_3D_data.dat","w");
+	fprintf(archivoSalida, "%f %f %f\n", gsl_vector_get(vx1,0), gsl_vector_get(vx1,1), gsl_vector_get(vx1,2));
+	fprintf(archivoSalida, "%f %f %f\n", gsl_vector_get(vx2,0), gsl_vector_get(vx2,1), gsl_vector_get(vx2,2));
+	fprintf(archivoSalida, "%f %f %f\n", gsl_vector_get(vx3,0), gsl_vector_get(vx3,1), gsl_vector_get(vx3,2));
+	fclose(archivoSalida);
+	return 0;
+	
+}
+
+// Función que cuenta el número de datos a procesar
+void contarDatos(int *nDatos, char nombreArchivo[])
+{
+	// Variables locales y abre el archivo
+	int numDatos = 0;
+	FILE *archivo;
+	archivo = fopen(nombreArchivo,"r");
+
+	char var = fgetc(archivo);
+	// Cuenta el número de datos
+	while (var != EOF)
+	{
+		if (var == '\n')
+			numDatos++;
+		var = fgetc(archivo);
+	}
+	
+	*nDatos = numDatos;
+	fclose(archivo);
+}
+
+
+gsl_vector* cargarVector(int nDatos, int numeroColumna, char nombreArchivo[])
+{
+	// Variables locales y abre el archivo
+	FILE *archivo2;
+	archivo2 = fopen(nombreArchivo,"r");
+
+	// Arreglo con los datos de la matriz
+	gsl_vector *vector = gsl_vector_calloc(nDatos);	
+
+	// Lee los datos y los guarda en la matriz
+	int filaActual = 0;
+	for (filaActual = 0; filaActual < nDatos; filaActual++)
+	{
+		double dato1, dato2, dato3;
+		fscanf(archivo2, "%lf %lf %lf\n", &dato1, &dato2, &dato3);
+		if (numeroColumna == 1)
+			gsl_vector_set(vector, filaActual, dato1);
+		else if (numeroColumna == 2)
+			gsl_vector_set(vector, filaActual, dato2);
+		else if (numeroColumna == 3)
+			gsl_vector_set(vector, filaActual, dato3);
+	}
+
+	fclose(archivo2);		
+	return vector;
+}
+
+// Calcula la media aritmética de un vector
+double calcularMedia(int tamanioVector, gsl_vector *vector)
+{
+	double suma = 0;
+	int i = 0;
+	for (i = 0; i < tamanioVector; i++)
+	{
+		suma += gsl_vector_get(vector, i);
+	}
+	return suma/tamanioVector;
+}
+
+// Calcular covarianza
+double calcularCovarianza(int tamanioVector, gsl_vector *v1, gsl_vector *v2, double u1, double u2)
+{
+	double suma = 0;
+	int i = 0;
+	for (i = 0; i < tamanioVector; i++)
+	{
+		suma += (gsl_vector_get(v1,i)-u1) * (gsl_vector_get(v2,i)-u2);
+	}
+	return suma/(tamanioVector-1);
+}
+
+// Calcula la norma de un vector
+double calcularNorma(int tamanioVector, gsl_vector *vector)
+{
+	double suma = 0;
+	int i = 0;
+	for (i = 0; i < tamanioVector; i++)
+	{
+		suma += pow((gsl_vector_get(vector,i)),2);
+	}
+	return pow(suma,0.5);
+}
+
